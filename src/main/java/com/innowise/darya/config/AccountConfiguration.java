ee22f18a9e71a1ba408e4f6a6dbf39b65d65ee6f
lindela5 <lindela@mail.ru>
2020-12-11 17:43:31 +0300

    login conf

--- src/main/java/com/innowise/darya/config/AccountConfiguration.java
+++ src/main/java/com/innowise/darya/config/AccountConfiguration.java
@@ -8,35 +8,55 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-//@EnableWebSecurity
-public class AccountConfiguration{// extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class AccountConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    UserDetailsService userDetailsService(AccountRepository accountRepository){
-        return username -> accountRepository
-                .findByUsername(username)
-                .map(account -> {
-                    boolean active = account.isActive();
-                    return new User(account.getUsername(), account.getPassword(), active,
-                            active, active,active, AuthorityUtils.createAuthorityList("ROLE_ADMIN",
-                            "ROLE_USER"));
-                })
-                .orElseThrow(
-                        ()-> new UsernameNotFoundException(String.format("username %s not found!",
-                               username )));
+//    @Bean
+//    UserDetailsService userDetailsService(AccountRepository accountRepository){
+//        return username -> accountRepository
+//                .findByUsername(username)
+//                .map(account -> {
+//                    boolean active = account.isActive();
+//                    return new User(account.getUsername(), account.getPassword(), active,
+//                            active, active, active, AuthorityUtils.createAuthorityList("ROLE_ADMIN",
+//                            "ROLE_USER"));
+//                })
+//                .orElseThrow(
+//                        ()-> new UsernameNotFoundException(String.format("username %s not found!",
+//                               username )));
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        super.configure(http);
-//    }
-//
-//    @Override
-//    protected UserDetailsService userDetailsService() {
-//        return super.userDetailsService();
-//    }
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 }
+
